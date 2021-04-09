3b0eddb13485b738c7bae918d59e7eda2b79fb70
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-04-09 11:46:25 -0700

    Bump to v1.0.5
    Bug Fixes:
    
    - Uses dialog-polyfill `v0.5.6`
    - Update build tools

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,14 @@ const json = require('./package.json')
 const getPath = path => path.substring(0, path.lastIndexOf('/'))
 const getFile = path => path.substring(path.lastIndexOf('/') + 1)
 const mode = process.env.NODE_ENV
-const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   plugins: [
-    new PeerDepsExternalsPlugin(),
+    new ESLintPlugin({
+      failOnWarning: true,
+      failOnError: true
+    })
   ],
   entry: `./${json.src}`,
   output: {
@@ -18,20 +21,10 @@ module.exports = {
     libraryTarget: 'umd',
     globalObject: 'this'
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals(), 'react'],
   mode: 'production',
   module: {
     rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "eslint-loader",
-        options: {
-          failOnWarning: true,
-          failOnError: true
-        }
-      },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
