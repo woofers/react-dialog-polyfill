ec2a24440b6a646ca9b4a9a1700d33d1e95c2b0e
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-03-13 12:37:04 -0700

    Code split styles

--- src/use-inject-style.js
+++ src/use-inject-style.js
@@ -1,25 +1,36 @@
 import { useEffect } from 'react'
 import { hasSupport } from './util'
-import css from './styles'
 
 let uses = 0
 const id = '_rdp'
 
 const hasDoc = () => typeof document !== 'undefined' && document.head
+const loadStyles = () => import('./styles').then(({ css }) => css)
 
 const useInjectStyle = () => {
   useEffect(() => {
     if (!hasDoc() || hasSupport()) return
     if (uses <= 0) {
-      const style = document.createElement('style')
-      style.innerHTML = css
-      style.id = id
-      document.head.appendChild(style)
+      let cancel = false
+      const injectStyle = async () => {
+        const style = document.createElement('style')
+        let css = ''
+        try {
+          css = await loadStyles()
+        } catch (e) {
+          cancel = true
+        }
+        style.innerHTML = css
+        style.id = id
+        if (!cancel) document.head.appendChild(style)
+      }
+      injectStyle()
     }
     uses++
     return () => {
       uses--
       if (!hasDoc() || uses > 0) return
+      cancel = true
       const element = document.getElementById(id)
       if (element) document.head.removeChild(element)
     }
